7158c0ff9f38a0aea4a8b54c0951324ca30a532d
Le Tan Khang <tankhang@ackcio.com>
2018-10-16 11:22:54 +0800

    Add Students class

--- SCRAME/src/Students.java
+++ SCRAME/src/Students.java
@@ -1,4 +1,39 @@
 
 public class Students {
 
+    private int studentID;
+    private String studentName;
+
+
+    //CONSTRUCTOR
+
+    /**
+     * constructor for student
+     * @param studentName name of the student
+     * @param studentID identification number of the student
+     */
+    public Students (String studentName, int studentID) {
+        this.studentName = studentName;
+        this.studentID = studentID;
+    }
+
+    //GET METHODS
+
+    /**
+     * Retrieves student identification number
+     * @return studentID
+     */
+    public int getStudentID() {
+        return studentID;
+    }
+
+    /**
+     * Retrieves the student's name for this instance
+     * @return name of the student
+     */
+    public String getStudentName() {
+        return studentName;
+    }
+
+
 }
